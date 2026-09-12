a35f256e8d45f85f8877b7f163c9c17b2e0a0b0f
あゆみ <ayumi.inutsuka@icloud.com>
2026-09-12 12:56:10 +0900

    パンフレット表記の追加

--- 1_作品保存.py
+++ 1_作品保存.py
@@ -6,7 +6,6 @@ if "movie_records" not in st.session_state:
 
 st.title("映画記録アプリ")
 
-
 movie_title = st.text_input("作品名")
 
 st.subheader("鑑賞日・方法")
@@ -24,14 +23,13 @@ elif movie_media == "動画配信サービス":
 elif movie_media == "映画館":
     movie_media = st.text_input("劇場名")
 
-
 st.subheader("作品評価")
 movie_point = st.slider("評価",0.0,5.0,step=0.5,format="%.1f")
 movie_review = st.text_area("感想")
 
 st.subheader("ファイルアップロード")
 movie_file = st.file_uploader(
-    "チケットをアップロード",
+    "チケット・パンフレット等をアップロード",
     type=["png","jpg","jpeg"],
     help="PNG,JPG,JPEG形式のみアップロード可能です")
 
