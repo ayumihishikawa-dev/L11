216f3a2711e6278b070065e6cc587d7ed6a6fa1c
あゆみ <ayumi.inutsuka@icloud.com>
2026-09-12 12:30:19 +0900

    動画配信サービス名の登録

--- 1_作品保存.py
+++ 1_作品保存.py
@@ -12,7 +12,12 @@ movie_title = st.text_input("作品名")
 st.subheader("鑑賞日・方法")
 movie_date  = st.date_input("鑑賞日")
 movie_media = st.radio("鑑賞方法",["動画配信サービス","映画館","試写会","DVD/ブルーレイ/VHS","TV地上波","BS/CS","飛行機","その他"])
-if movie_media == "その他":
+if movie_media == "動画配信サービス":
+    movie_service = st.selectbox("配信サービス",["U-NEXT","Amazonプライム","Hulu","Netflix","ディズニープラス","その他"])
+    if movie_service == "その他":
+        movie_service = st.text_input("サービス名")
+
+elif movie_media == "その他":
     movie_media = st.text_input("鑑賞方法は？")
 
 st.subheader("作品評価")
